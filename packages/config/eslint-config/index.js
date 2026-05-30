67bc251fb042e75c04b6cb54bb50b2de0cbca501
Tommaso Lisella <tommasolisella99@gmail.com>
2026-05-30 18:28:24 +0200

    update eslint config

--- packages/config/eslint-config/index.js
+++ packages/config/eslint-config/index.js
@@ -9,166 +9,162 @@ import perfectionist from 'eslint-plugin-perfectionist';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-/**
- * @type {ConfigWithExtendsArray}
- */
-const defaultConfig = [
-  js.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
+const typescriptRules = {
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
       },
-    },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        disallowTypeAnnotations: true,
+        fixStyle: 'separate-type-imports',
+        prefer: 'type-imports',
+      },
+    ],
   },
-  {
-    rules: {
-      'no-console': [
-        'error',
-        {
-          allow: ['warn', 'error'],
-        },
-      ],
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          args: 'all',
-          argsIgnorePattern: '^_',
-          caughtErrors: 'all',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-        },
-      ],
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        {
-          disallowTypeAnnotations: true,
-          fixStyle: 'separate-type-imports',
-          prefer: 'type-imports',
-        },
-      ],
-    },
+};
+
+const perfectionistConfig = {
+  plugins: {
+    perfectionist,
   },
-];
+  rules: {
+    'perfectionist/sort-exports': [
+      'error',
+      {
+        type: 'natural',
+        order: 'asc',
+        fallbackSort: { type: 'alphabetical' },
+        ignoreCase: true,
+        specialCharacters: 'keep',
+        partitionByComment: false,
+        partitionByNewLine: false,
+        newlinesBetween: 'ignore',
+        newlinesInside: 'ignore',
+        groups: [],
+        customGroups: [],
+      },
+    ],
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        type: 'natural',
+        order: 'asc',
+        fallbackSort: { type: 'alphabetical' },
+        ignoreCase: true,
+        specialCharacters: 'keep',
+        sortBy: 'path',
+        internalPattern: ['^~/.+', '^@/.+', '^#.+'],
+        partitionByComment: false,
+        partitionByNewLine: false,
+        newlinesBetween: 1,
+        newlinesInside: 0,
+        maxLineLength: undefined,
+        groups: [
+          'react',
+          'type-import',
+          ['value-builtin', 'value-external'],
+          'type-internal-packages',
+          'value-internal-packages',
+          'type-internal',
+          'value-internal',
+          ['type-parent', 'type-sibling', 'type-index'],
+          ['value-parent', 'value-sibling', 'value-index'],
+          'ts-equals-import',
+          'unknown',
+        ],
+        customGroups: [
+          {
+            groupName: 'react',
+            elementNamePattern: ['^react$', '^react-.+'],
+          },
+          {
+            selector: 'type',
+            groupName: 'type-internal-packages',
+            elementNamePattern: ['^#pkg/.+', '^@pkg/.+'],
+          },
+          {
+            groupName: 'value-internal-packages',
+            elementNamePattern: ['^#pkg/.+', '^@pkg/.+'],
+          },
+        ],
+        environment: 'bun',
+        useExperimentalDependencyDetection: true,
+      },
+    ],
+    'perfectionist/sort-named-exports': [
+      'error',
+      {
+        type: 'natural',
+        order: 'asc',
+        fallbackSort: { type: 'alphabetical' },
+        ignoreAlias: false,
+        ignoreCase: true,
+        specialCharacters: 'keep',
+        partitionByNewLine: false,
+        partitionByComment: false,
+        newlinesBetween: 'ignore',
+        newlinesInside: 'ignore',
+        groups: [],
+        customGroups: [],
+      },
+    ],
+    'perfectionist/sort-named-imports': [
+      'error',
+      {
+        type: 'alphabetical',
+        order: 'asc',
+        fallbackSort: { type: 'unsorted' },
+        ignoreAlias: false,
+        ignoreCase: true,
+        specialCharacters: 'keep',
+        partitionByNewLine: false,
+        partitionByComment: false,
+        newlinesBetween: 'ignore',
+        newlinesInside: 'ignore',
+        groups: [],
+        customGroups: [],
+      },
+    ],
+  },
+};
 
 /**
- * @type {ConfigWithExtendsArray}
- */
-const perfectionistConfig = [
-  {
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'perfectionist/sort-exports': [
-        'error',
-        {
-          type: 'natural',
-          order: 'asc',
-          fallbackSort: { type: 'alphabetical' },
-          ignoreCase: true,
-          specialCharacters: 'keep',
-          partitionByComment: false,
-          partitionByNewLine: false,
-          newlinesBetween: 'ignore',
-          newlinesInside: 'ignore',
-          groups: [],
-          customGroups: [],
-        },
-      ],
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          type: 'natural',
-          order: 'asc',
-          fallbackSort: { type: 'alphabetical' },
-          ignoreCase: true,
-          specialCharacters: 'keep',
-          sortBy: 'path',
-          internalPattern: ['^~/.+', '^@/.+', '^#.+'],
-          partitionByComment: false,
-          partitionByNewLine: false,
-          newlinesBetween: 1,
-          newlinesInside: 0,
-          maxLineLength: undefined,
-          groups: [
-            'react',
-            'type-import',
-            ['value-builtin', 'value-external'],
-            'type-internal-packages',
-            'value-internal-packages',
-            'type-internal',
-            'value-internal',
-            ['type-parent', 'type-sibling', 'type-index'],
-            ['value-parent', 'value-sibling', 'value-index'],
-            'ts-equals-import',
-            'unknown',
-          ],
-          customGroups: [
-            {
-              groupName: 'react',
-              elementNamePattern: ['^react$', '^react-.+'],
-            },
-            {
-              selector: 'type',
-              groupName: 'type-internal-packages',
-              elementNamePattern: ['^#pkg/.+', '^@pkg/.+'],
-            },
-            {
-              groupName: 'value-internal-packages',
-              elementNamePattern: ['^#pkg/.+', '^@pkg/.+'],
-            },
-          ],
-          environment: 'bun',
-          useExperimentalDependencyDetection: true,
-        },
-      ],
-      'perfectionist/sort-named-exports': [
-        'error',
-        {
-          type: 'natural',
-          order: 'asc',
-          fallbackSort: { type: 'alphabetical' },
-          ignoreAlias: false,
-          ignoreCase: true,
-          specialCharacters: 'keep',
-          partitionByNewLine: false,
-          partitionByComment: false,
-          newlinesBetween: 'ignore',
-          newlinesInside: 'ignore',
-          groups: [],
-          customGroups: [],
-        },
-      ],
-      'perfectionist/sort-named-imports': [
-        'error',
-        {
-          type: 'alphabetical',
-          order: 'asc',
-          fallbackSort: { type: 'unsorted' },
-          ignoreAlias: false,
-          ignoreCase: true,
-          specialCharacters: 'keep',
-          partitionByNewLine: false,
-          partitionByComment: false,
-          newlinesBetween: 'ignore',
-          newlinesInside: 'ignore',
-          groups: [],
-          customGroups: [],
-        },
-      ],
-    },
-  },
-];
-
-/**
- * @param {ConfigWithExtendsArray} config
+ * @param {ConfigWithExtendsArray} args
  * @returns {Config[]}
  */
-export const defineExtraConfig = (...config) =>
-  defineConfig([...defaultConfig, ...perfectionistConfig, ...config, prettier]);
+export const defineExtraConfig = (...args) =>
+  defineConfig([
+    {
+      files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+      plugins: { js },
+      extends: ['js/recommended'],
+      rules: {
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+      },
+    },
+    tseslint.configs.recommendedTypeChecked,
+    {
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+        },
+      },
+    },
+    typescriptRules,
+    perfectionistConfig,
+    ...args,
+    prettier,
+  ]);
 
 export default defineExtraConfig();
